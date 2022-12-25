188f5d7d55c61d720bab6bf58c484e262baa335f
Fathoni Zikri Nugroho <fathonizikri11@gmail.com>
2022-12-25 16:22:18 +0700

    repair router

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.redirect("/admin/signin");
 });
 
 router.use("/api/v1", apiRouter);
